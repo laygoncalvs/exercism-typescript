9515e3f8df9500a8b762e2bab25afd2648dc383c
Layanne <layannegoncalveslg@gmail.com>
2026-07-24 23:18:18 -0300

    feat:line-up

---
+++ line-up/line-up.ts
@@ -0,0 +1,18 @@
+export function format(name: string, number: number): string {
+  let suffix = 'th'
+
+  const lastTwoDigits = number % 100
+  const lastDigit = number % 10
+
+  if (lastTwoDigits !== 11 && lastTwoDigits !== 12 && lastTwoDigits !== 13) {
+    if (lastDigit === 1) {
+      suffix = 'st'
+    } else if (lastDigit === 2) {
+      suffix = 'nd'
+    } else if (lastDigit === 3) {
+      suffix = 'rd'
+    }
+  }
+
+  return `${name}, you are the ${number}${suffix} customer we serve today. Thank you!`
+}
