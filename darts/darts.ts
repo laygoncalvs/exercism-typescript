2659e64c5f9aef5b6bc83c902373ce1b49f91310
Layanne <layannegoncalveslg@gmail.com>
2026-09-18 13:31:10 -0300

    feat: complete darts exercise

---
+++ darts/darts.ts
@@ -0,0 +1,17 @@
+export function score(x: number, y: number): number {
+  const distance = Math.sqrt(x ** 2 + y ** 2)
+
+  if (distance <= 1) {
+    return 10
+  }
+
+  if (distance <= 5) {
+    return 5
+  }
+
+  if (distance <= 10) {
+    return 1
+  }
+
+  return 0
+}
